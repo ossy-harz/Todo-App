b078579e2727913bd8a8e2c38fe5caee97dc3a07
ossy-harz <harz31600@gmail.com>
2025-07-30 16:53:30 +0300

    Initial commit of Flutter mobile app

--- mobile_app/lib/features/tasks/domain/repositories/task_repository.dart
+++
@@ -1,9 +0,0 @@
-import '../entities/task.dart';
-
-abstract class TaskRepository {
-  Stream<List<Task>> getTasks();
-  Future<void> addTask(Task task);
-  Future<void> updateTask(Task task);
-  Future<void> deleteTask(String taskId);
-  Future<Task?> getTask(String taskId);
-}
